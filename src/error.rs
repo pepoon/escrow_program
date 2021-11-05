978dc3a404d37100a40720a93adb842beb88cdf4
josep <calullmercade@gmail.com>
2021-11-05 13:02:12 +0100

    exchange function

---
+++ src/error.rs
@@ -0,0 +1,21 @@
+use solana_program::program_error::ProgramError;
+// inside error.rs
+use thiserror::Error;
+
+#[derive(Error, Debug, Copy, Clone)]
+pub enum EscrowError {
+    /// Invalid instruction
+    #[error("Invalid Instruction")]
+    InvalidInstruction,
+    #[error("Not Rent Exempt")]
+    NotRentExempt,
+    #[error("Expected Amount Mismatch")]
+    ExpectedAmountMismatch,
+    #[error("Amount Overflow")]
+    AmountOverflow
+}
+impl From<EscrowError> for ProgramError {
+    fn from(e: EscrowError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
